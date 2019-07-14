a34e8d44e6c17825d5a33fe03e32ecebd9770c87
Chris6002 <qsun15@u.rochester.edu>
2019-07-14 17:36:45 -0400

    first

---
+++ sphereNet.py
@@ -0,0 +1,169 @@
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+import vgg
+
+plt.ion()  
+
+use_gpu = torch.cuda.is_available()
+if use_gpu:
+    print("Using CUDA")
+
+data_dir = '../input/kermany2018/oct2017/OCT2017 '
+
+TRAIN = 'train'
+VAL = 'val'
+TEST = 'test'
+
+# VGG-16 Takes 224x224 images as input, so we resize all of them
+data_transforms = {
+    TRAIN: transforms.Compose([
+        # Data augmentation is a good practice for the train set
+        # Here, we randomly crop the image to 224x224 and
+        # randomly flip it horizontally. 
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ]),
+    VAL: transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ]),
+    TEST: transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+    ])
+}
+
+image_datasets = {
+    x: datasets.ImageFolder(
+        os.path.join(data_dir, x), 
+        transform=data_transforms[x]
+    )
+    for x in [TRAIN, VAL, TEST]
+}
+
+dataloaders = {
+    x: torch.utils.data.DataLoader(
+        image_datasets[x], batch_size=8,
+        shuffle=True, num_workers=4
+    )
+    for x in [TRAIN, VAL, TEST]
+}
+
+dataset_sizes = {x: len(image_datasets[x]) for x in [TRAIN, VAL, TEST]}
+
+for x in [TRAIN, VAL, TEST]:
+    print("Loaded {} images under {}".format(dataset_sizes[x], x))
+    
+print("Classes: ")
+class_names = image_datasets[TRAIN].classes
+
+def visualize_model(vgg, num_images=6):
+    was_training = vgg.training
+    
+    # Set model for evaluation
+    vgg.train(False)
+    vgg.eval() 
+    
+    images_so_far = 0
+
+    for i, data in enumerate(dataloaders[TEST]):
+        inputs, labels = data
+        size = inputs.size()[0]
+        
+        if use_gpu:
+            inputs, labels = Variable(inputs.cuda(), volatile=True), Variable(labels.cuda(), volatile=True)
+        else:
+            inputs, labels = Variable(inputs, volatile=True), Variable(labels, volatile=True)
+        
+        outputs = vgg(inputs)
+        
+        _, preds = torch.max(outputs.data, 1)
+        predicted_labels = [preds[j] for j in range(inputs.size()[0])]
+        
+        print("Ground truth:")
+        show_databatch(inputs.data.cpu(), labels.data.cpu())
+        print("Prediction:")
+        show_databatch(inputs.data.cpu(), predicted_labels)
+        
+        del inputs, labels, outputs, preds, predicted_labels
+        torch.cuda.empty_cache()
+        
+        images_so_far += size
+        if images_so_far >= num_images:
+            break
+        
+    vgg.train(mode=was_training) # Revert model back to original training state
+
+def eval_model(vgg, criterion):
+    since = time.time()
+    avg_loss = 0
+    avg_acc = 0
+    loss_test = 0
+    acc_test = 0
+    
+    test_batches = len(dataloaders[TEST])
+    print("Evaluating model")
+    print('-' * 10)
+    
+    for i, data in enumerate(dataloaders[TEST]):
+        if i % 100 == 0:
+            print("\rTest batch {}/{}".format(i, test_batches), end='', flush=True)
+
+        vgg.train(False)
+        vgg.eval()
+        inputs, labels = data
+
+        if use_gpu:
+            inputs, labels = Variable(inputs.cuda(), volatile=True), Variable(labels.cuda(), volatile=True)
+        else:
+            inputs, labels = Variable(inputs, volatile=True), Variable(labels, volatile=True)
+
+        outputs = vgg(inputs)
+
+        _, preds = torch.max(outputs.data, 1)
+        loss = criterion(outputs, labels)
+
+        loss_test += loss.data[0]
+        acc_test += torch.sum(preds == labels.data)
+
+        del inputs, labels, outputs, preds
+        torch.cuda.empty_cache()
+        
+    avg_loss = loss_test / dataset_sizes[TEST]
+    avg_acc = acc_test / dataset_sizes[TEST]
+    
+    elapsed_time = time.time() - since
+    print()
+    print("Evaluation completed in {:.0f}m {:.0f}s".format(elapsed_time // 60, elapsed_time % 60))
+    print("Avg loss (test): {:.4f}".format(avg_loss))
+    print("Avg acc (test): {:.4f}".format(avg_acc))
+    print('-' * 10)
+
+def main():
+	# Load the pretrained model from pytorch
+	vgg16 = vgg16_bn(True)
+	vgg16.load_state_dict(torch.load("../input/vgg16bn/vgg16_bn.pth"))
+
+	# Freeze training for all layers
+	for param in vgg16.features.parameters():
+	    param.require_grad = False
+
+	criterion = nn.CrossEntropyLoss()
+	eval_model(vgg16, criterion)
+
+main()
